517161c2c5868880ddeb84e514ac6742b87715fd
Nero <137304300+NeroClaudia@users.noreply.github.com>
2026-04-06 20:37:10 +0700

    Fixing error nested git

---
+++ week05/src/Student07.java
@@ -0,0 +1,21 @@
+public class Student07 {
+
+    String nim, name, studentClass;
+    double gpa;
+
+    public Student07() {
+
+    }
+
+    public Student07(String nim, String name, String studentClass, double gpa) {
+        this.nim = nim;
+        this.name = name;
+        this.studentClass = studentClass;
+        this.gpa = gpa;
+    }
+
+    public void print() {
+        System.out.println("NIM: " + nim + ", Name: " + name + ", Class: " + studentClass + ", GPA: " + gpa);
+    }
+
+}
