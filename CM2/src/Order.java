11ba79e86eeb57e544a98f17efe96cdae666fbea
Nero <137304300+NeroClaudia@users.noreply.github.com>
2026-05-20 21:58:56 +0700

    Add: calculateTotal method and removeItem method

--- CM2/src/Order.java
+++ CM2/src/Order.java
@@ -18,7 +18,48 @@ public class Order {
             tail = newNode;
         }
     }
+    
+    double calculateTotal() {
+        double total = 0;
+        OrderNode current = head;
 
+        while (current != null) {
+            total += current.price;
+            current = current.next;
+        }
+        return total;
+    }
+    
+    void removeItem(String orderName) {
+        if (head == null) {
+            System.out.println("Pesanan Kosong");
+            return;
+        }
+        OrderNode current = head;
+
+        while (current != null) {
+            if (current.orderName.equalsIgnoreCase(orderName)) {
+                if (current == head && current == tail) {
+                    head = null;
+                    tail = null;
+                }else if (current == head) {
+                    head = head.next;
+                    head.prev = null;
+                }else if (current == tail) {
+                    tail = tail.prev;
+                    tail.next = null;
+                }else {
+                    current.prev.next = current.next;
+                    current.next.prev = current.prev;
+                }
+                System.out.println("Item '" + current.orderName + "' berhasil dihapus dari daftar pesanan.");
+                return;
+            }
+            current = current.next;
+        }
+        System.out.println("Item " + orderName + " tidak ditemukan");
+    }
+    
     void displayItems() {
         OrderNode current = head;
         while (current != null) {
