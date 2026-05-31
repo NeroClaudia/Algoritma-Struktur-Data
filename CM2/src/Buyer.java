ec194b24cd0a8743ccbb99b71bea2b3e128b4ddc
Nero <137304300+NeroClaudia@users.noreply.github.com>
2026-05-31 21:42:19 +0700

    Refactor: apply encapsulation to Buyer class

--- CM2/src/Buyer.java
+++ CM2/src/Buyer.java
@@ -1,10 +1,49 @@
 public class Buyer {
 
-    int queueNumber;
-    String name, mobileNumber;
+    private int queueNumber;
+    private String name;
+    private String mobileNumber;
 
     public Buyer(String name, String mobileNumber) {
-        this.name = name;
-        this.mobileNumber = mobileNumber;
+        setName(name);
+        setMobileNumber(mobileNumber);
+        this.queueNumber = 0;
+    }
+
+    public int getQueueNumber() {
+        return queueNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setQueueNumber(int queueNumber) {
+        if (queueNumber > 0) {
+            this.queueNumber = queueNumber;
+        }
+    }
+
+    public void setName(String name) {
+        if (name != null && !name.isEmpty()) {
+            this.name = name;
+        }
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        if (mobileNumber != null && !mobileNumber.isEmpty()) {
+            this.mobileNumber = mobileNumber;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Buyer{queueNumber=" + queueNumber +
+               ", name='" + name + "'" +
+               ", mobileNumber='" + mobileNumber + "'}";
     }
 }
